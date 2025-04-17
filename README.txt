c1f34bc68e29e8fbb559bf92fdd8a959e587e75e
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2025-04-17 20:57:02 +0200

    template

---
+++ README.txt
@@ -0,0 +1,35 @@
+NodeJs
+         https://nodejs.org/en/
+
+Vscode
+         https://code.visualstudio.com/
+
+Install
+         npm install -g @angular/cli@13
+         ng version
+         ng new helloworld
+         cd helloworld
+         code .
+         ng serve
+
+Component
+         ng g c name_component
+
+Bootstrap
+         npm install bootstrap
+         "node_modules/bootstrap/dist/css/bootstrap.min.css" (angular.json)
+
+
+Firebase
+         npm install firebase@9 --save
+         npm install --save  @angular/fire@7
+
+Service
+         ng g s auth
+           
+Model
+           ng g class nom_model
+
+
+Angular Material
+           ng add @angular/material 
--- helloworld/src/app/app.module.ts
+++ helloworld/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule} from '@angular/fire/compat/database';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { TestComponent } from './test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
